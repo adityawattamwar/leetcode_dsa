91ba3f7b34b780e0b37616a60c511adfa491eb19
adityawattamwar <121751089+adityawattamwar@users.noreply.github.com>
2024-04-06 00:00:39 +0530

    Time: 84 ms (43.06%) | Memory: 41.2 MB (97.58%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,21 +1,27 @@
 class MyCalendar {
 private:
     vector<pair<int, int>> l;
+    int cnt=0;
 public:
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        bool flag = true;
+        bool flag = false;
         for(auto a : l) {
-            if(start < a.second && end > a.first) {
-                flag = false;
+            if(start >= a.second or end <= a.first) {
+                flag = true;
+                
+            }
+            else{
+                flag=false;
                 break;
             }
         }
-        if(flag) {
+        if(flag || cnt==0) {
             l.push_back(make_pair(start, end));
+            cnt++;
             return true;
         }
         return false;
