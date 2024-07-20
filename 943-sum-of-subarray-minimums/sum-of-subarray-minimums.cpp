d1238cf5d36e3d4b97fc4d59e51559da50c42155
adityawattamwar <121751089+adityawattamwar@users.noreply.github.com>
2024-07-20 14:21:44 +0530

    Time: 62 ms (71.13%) | Memory: 45.7 MB (34.95%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
-        int mod = 1e9+7;
-        vector<int64_t> smaller_right(arr.size(), arr.size());
-        // mono increasing stack.
-        stack<int64_t> st;
-        for(int i = arr.size() - 1; i >= 0; --i){
-            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            if(!st.empty()) smaller_right[i] = st.top();
-            st.push(i);
-        }
-        st = {};
-        int res = 0;
-        for(int i = 0; i < arr.size(); ++i){
-            while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
-            int left = st.empty() ? -1 : st.top();
-            res = (res + (arr[i] * (i - left) * (smaller_right[i] - i)) % mod) % mod;
-            st.push(i);
-        }
-        return res;
+    vector<int> findnse(vector<int> &arr){
+        int n = arr.size();
+        vector<int> nse(n);
+        stack<int> st1;
 
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st1.empty() && arr[st1.top()] >= arr[i])
+                st1.pop();
+            nse[i] = st1.empty() ? n : st1.top();
+            st1.push(i);    
+        }
+        return nse;
+
+    }
+    vector<int> findpsee(vector<int> &arr){
+        int n = arr.size();
+        vector<int> pse(n);
+        stack<int> st2;
+
+        for(int i = 0 ; i <n ; i++){
+            while(!st2.empty() && arr[st2.top()] > arr[i])
+                st2.pop();
+            pse[i] = st2.empty() ? -1 : st2.top();
+            st2.push(i);    
+        }
+        return pse;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+
+        vector<int> nse = findnse(arr);
+        vector<int> psee = findpsee(arr);
+        int total = 0; 
+        int mod = (int)(1e9 + 7);
+        for(int i = 0 ; i < arr.size() ; i++){
+            int left = i - psee[i];
+            int right = nse[i] - i;
+
+            total = (total + (right*left* 1LL * arr[i] )%mod)%mod ;
+
+
+        }
+        return total;
     }
 };
