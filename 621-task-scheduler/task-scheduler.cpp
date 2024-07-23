685b7c06af07d3c7cc74d975297a47889d99c954
adityawattamwar <121751089+adityawattamwar@users.noreply.github.com>
2024-07-23 20:21:48 +0530

    Time: 62 ms (61.97%) | Memory: 41.8 MB (43.08%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>mp;
-        int count = 0;
-        for(auto e : tasks)
-        {
-            mp[e]++;
-            count = max(count, mp[e]);
+        unordered_map<char, int> mp;
+        priority_queue<int> pq;
+        
+        for (char task : tasks) {
+            mp[task]++;
         }
         
-        int ans = (count-1)*(n+1);
-        for(auto e : mp) if(e.second == count) ans++;
-        return max((int)tasks.size(), ans);
-
+        for (auto it : mp) {
+            pq.push(it.second);
+        }
+        
+        int time = 0;
+        queue<pair<int, int>> q;
+        
+        while (!pq.empty() || !q.empty()) {
+            if (!pq.empty()) {
+                int count = pq.top();
+                pq.pop();
+                count--;
+                
+                if (count > 0) {
+                    q.push({count, time + n});
+                }
+            }
+            
+            if (!q.empty() && q.front().second == time) {
+                pq.push(q.front().first);
+                q.pop();
+            }
+            
+            time++;
+        }
+        
+        return time;
     }
 };
