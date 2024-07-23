a082d4efb10d1fc7268ee9f57617d4814fae2355
adityawattamwar <121751089+adityawattamwar@users.noreply.github.com>
2024-07-23 21:46:38 +0530

    Time: 217 ms (5.02%) | Memory: 30.7 MB (27.29%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -3,43 +3,44 @@ public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if(hand.size()%groupSize!=0){
             return false;
+
         }
-        sort(hand.begin(),hand.end());
-        map<int,int> mp;
+        int act=hand.size()/groupSize;
+        unordered_map<int,int> mp;
         for(int i=0;i<hand.size();i++){
             mp[hand[i]]++;
         }
-        vector<int> temp;
-        
-        int cnt=groupSize;
-        cnt--;
-        temp.push_back(hand[0]);
-        mp[hand[0]]--;
-        for(int i=1;i<hand.size();i++){
-           if(cnt==0){
-               cnt=groupSize;
-               temp.clear();
-               cnt--;
-               for(auto it:mp){
-                if(it.second>0){
-                      temp.push_back(it.first);
-                      
-                      mp[it.first]--;
-                      break;
+        int grp=0,cnt=0;
+        int mini=INT_MAX;
+        int val=0;
+        while(grp<act){
+            cnt=0;
+            val=INT_MAX;
+            mini=INT_MAX;
+            for(auto i:mp){
+                if(i.second>0){
+                val=min(val,i.first);
                 }
+            }
+            cnt++;
+
+            mp[val]--;
+            while(cnt<groupSize){
+               val++;
+               if(mp[val]>0){
+                 mp[val]--;
+                
+                 cnt++;
                }
-               
-           }
-           else if(mp[temp.back()+1]==0){
-            // cout<<i<<endl;
-            return false;
-           }
-           else{
-               mp[temp.back()+1]--;
-               temp.push_back(temp.back()+1);
-               cnt--;
-               
-           }
+               else{
+                return false;
+               }
+
+            }
+            if(cnt==groupSize){
+                grp++;
+            }
+
         }
         return true;
 
