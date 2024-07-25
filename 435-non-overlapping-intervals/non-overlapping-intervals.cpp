2e3eb49b9c343dc32bbcfc3f355881549b7a021e
adityawattamwar <121751089+adityawattamwar@users.noreply.github.com>
2024-07-25 14:23:39 +0530

    Time: 237 ms (72.48%) | Memory: 93.7 MB (35.76%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,19 +1,26 @@
-bool comp(vector<int> &a,vector<int> &b) {
-	return a[1]<b[1];
-}
 class Solution {
 public:
-	int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-		int ans=-1;      
-		if(intervals.size()==0) return 0;       
-		sort(intervals.begin(),intervals.end(),comp);      //custom comperator is used.
-		vector<int> prev= intervals[0];
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if(intervals.empty()) return 0;
 
-		for(vector<int> i: intervals) {
-			if(prev[1]>i[0]) {
-				ans++;                //we dont update previous, because i[1] will be grater then prev[1]
-			}else prev=i;           // we want the end point to be minimum
-		}
-		return ans;                 //ans was initially made -1 because our prev and intervals[0] will always match
-	}
+        // Sort intervals by their end times
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] < b[1];
+        });
+
+        int count = 0;
+        int end = intervals[0][1];
+
+        for(int i = 1; i < intervals.size(); ++i) {
+            if(intervals[i][0] < end) {
+                // Overlapping interval
+                ++count;
+            } else {
+                // Non-overlapping interval, update the end
+                end = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
 };
