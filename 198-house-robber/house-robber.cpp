c8411cb8eeacf8ade27c70bcdebb7dbfed9bfff5
adityawattamwar <121751089+adityawattamwar@users.noreply.github.com>
2024-07-28 15:01:56 +0530

    Time: 2 ms (39.81%) | Memory: 9.6 MB (6.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,9 +9,22 @@ public:
 
     }
     int rob(vector<int>& nums) {
-        int ind=nums.size()-1;
         vector<int> dp(nums.size()+1,-1);
-        return find(dp,nums,ind);
+        dp[0]=nums[0];
+        
+        for(int i=0;i<nums.size();i++){
+            int pick=nums[i],notpick=0;
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            if(i>0){
+                 notpick=dp[i-1];
+            }
+            
+            dp[i]=max(pick,notpick);
+        }
+        return dp[nums.size()-1];
+        
         
 
     }
